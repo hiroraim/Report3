35e88a8586c10c7a3ed1927a4e1b7f8902890df3
Hiroto Shimabukuro <e175765@ie.u-ryukyu.ac.jp>
2017-11-12 00:07:39 +0900

    secoud style

--- src/test/jp/ac/uryukyu/ie/e175765/EnemyTest.java
+++ src/test/jp/ac/uryukyu/ie/e175765/EnemyTest.java
@@ -5,11 +5,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class EnemyTest {
+    @Test
+    public void attack() {
     /**
      * 「死んでる状態では攻撃できない」ことを確認。
      */
-    @Test
-    public void attack() {
+
+
         int heroHP = 10;
         Hero hero = new Hero("テスト勇者", heroHP, 5);
         Enemy enemy = new Enemy("テストスライム", 6, 3);
